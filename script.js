f86392dc1955c899e92f4bac4c4916b3d04dca82
feridani1307-collab <feridani1307@gmail.com>
2025-09-30 14:11:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton);
 tombolSapa.addEventListener('click', function() {
-   alert('halo! Terima kasih sudah berkunjung!');});
+    alert('halo! Terima kasih sudah berkunjung!');
+});
